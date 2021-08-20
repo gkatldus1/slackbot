602ce82486a7e8747299330d1496899b30947350
siyeon <gkatldus1@gmail.com>
2021-08-20 16:06:53 +0900

    write-channel

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ dotenv.config()
 
 const app = new App({
     token: `${process.env.BOT_TOKEN}`,
-    signingSecret: `${process.env.SLACK_SIGNING_SECRET}`
+    signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+    socketMode: true,
+    appToken: `${process.env.SLACK_APP_TOKEN}`
 });
+
+// app.message('hello', async ({ message, say}) => {
+//     await say(`Hey there <@${message.user}>!`);
+// })
+
+app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hey there <@${message.user}>!`);
+  });
 
 (async () => {
     await app.start(process.env.PORT || 3000);
 
     console.log('⚡️ Bolt app is running!');
-})
+})();
