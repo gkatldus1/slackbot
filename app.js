7e24de07bb5cb1ad3e24291e1a2159c905710333
siyeon <gkatldus1@gmail.com>
2021-08-25 11:28:41 +0900

    slackbot

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { App } = require('@slack/bolt');
-const dotenv = require('dotenv')
-const { WebClient, LogLevel } = require("@slack/web-api");
+const dotenv = require('dotenv');
+const fetch = require("node-fetch");
+const cron = require("node-cron");
 dotenv.config()
 
 const app = new App({
@@ -10,22 +11,57 @@ const app = new App({
     appToken: `${process.env.SLACK_APP_TOKEN}`
 });
 
-const client = new WebClient(`${process.env.BOT_TOKEN}`, {
-    // LogLevel can be imported and used to make debugging simpler
-    logLevel: LogLevel.DEBUG
-  });
-
-
 
 let food_arr = ['짜장면', '비빔밥', '칼국수', '백반', '카레', '고무고무열매', '이글이글열매', '도톰도톰열매'];
-let wanna_eat = [];
+let wanna_eat = ['홍길동', '김선비', '최규옹', '김민완', '이규아', '함시현'];
+let record = {'홍길동':['짜장면', '비빔밥', '칼국수'], '김선비':['짜장면', '비빔밥', '칼국수'], '최규옹':['짜장면', '비빔밥', '칼국수'], '김민완':['짜장면', '비빔밥', '칼국수'],'이규아':['짜장면', '비빔밥', '칼국수'], '함시현':['짜장면', '비빔밥', '칼국수']};
+let flag = false;
 
 
 app.message('!점심', async({ message, say }) => {
-    let random = Math.floor(Math.random() * 8);
+    let suggestion = '';
+    while(1){
+      
+      let menu = food_arr;
+      let random = Math.floor(Math.random() * 8);
+      let food = menu[random];
+      
+      for (let i = 0; i < wanna_eat.length; i++){
+        let user = record[wanna_eat[i]];
+        
+        if(user){
+          result = user.includes(food);
+          if(result === true){
+            flag = false;
+            let index = menu.indexOf(food);
+            if (index > -1) {
+              menu.splice(index, 1);
+            }
+            break;
+          } else {
+            flag = true;
+          } 
 
+        } 
+          
+      }
+      if (flag === true) {
+        flag = false;
+        suggestion = food;
+        for (let i = 0; i < wanna_eat.length; i++){
+            let user = record[wanna_eat[i]];
+            user.shift();
+            user.push(food);
+        }
+        break;
+    }
+      
+
+    }
+    console.log(suggestion);
+  
     await say({
-        text:`오늘 점심은 ${food_arr[random]}을(를) 추천드립니다.`
+        text:`오늘 점심은 ${suggestion}을(를) 추천드립니다.`
     });
 });
 
@@ -50,71 +86,68 @@ app.action('count_clicker', async ({ body, ack, say }) => {
 });
 
 
-// 12:20 sending message logic
-let lunchtime = '1629775200';
-let temp = '11629707020';
-const next = 86400;
+
 // Channel you want to post the message to
 const channelId = "C02C13NGFU1";
 
-const today = new Date();
-today.setDate(today.getDate());
-today.setHours(5,17, 0);
-
-
-try {
-    // Call the chat.scheduleMessage method using the WebClient
-     const result = async () => {
-         await client.chat.scheduleMessage({
-            blocks: [
-                {
-                  "type": "section",
-                  "text": {
-                    "type": "mrkdwn",
-                    "text": `현재 식사를 원하는 인원은 ${wanna_eat.length} 명 입니다.`
-                  },
-                  "accessory": {
-                    "type": "button",
-                    "text": {
-                      "type": "plain_text",
-                      "text": "식사를 원하시면 눌러주세요"
-                    },
-                    "action_id": "count_clicker"
-                  }
-                }
-              ],
-            channel: channelId,
-            text: "Looking towards the future",
-            // Time to post message, in Unix Epoch timestamp format
-            post_at: temp /1000
-            });
-        }
-  
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-finally{
-    lunchtime = lunchtime + next;
-}
-
-
-try {
-  // Call the chat.postMessage method using the WebClient
-  const result = async() => {
-    await client.chat.postMessage({
+const payload = {
     channel: channelId,
-    text: "Hello world"
+    blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": '식사를 원하시는 분은 버튼을 눌러주세요~'
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "식사를 원하시면 눌러주세요"
+            },
+            "action_id": "count_clicker"
+          }
+        }
+      ],
+    attachments: [
+      {
+        title: "점심시간 10분 전!!!",
+        // text: "점심시간 10분 전!!!",
+        // author_name: "Food Fairy",
+        color: "#00FF00",
+      },
+    ],
+  };
+  
+
+  const task = cron.schedule("10 12 1-31 * *", () => {
+    fetch("https://slack.com/api/chat.postMessage", {
+    
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Content-Length": payload.length,
+      Authorization: `Bearer ${process.env.BOT_TOKEN}`,
+      Accept: "application/json",
+      
+    },
+    
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Server error ${res.status}`);
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, {
+    scheduled: false
   });
-}
-
-  console.log(result);
-}
-catch (error) {
-  console.error(error);
-}
-
+ 
+  task.start();
 
 (async () => {
     await app.start(process.env.PORT || 3000);
